50eeabcf6f9aa4eaf4f0aa1a4aadb47ca048864a
smhigley <shigley@sitepen.com>
2017-08-08 17:54:55 -0700

    step 4 code

--- src/index.js
+++ src/index.js
@@ -8,15 +8,11 @@ const svg = d3.select('svg'),
     height = +svg.attr('height');
 
 // save reference to DOM elements
-const dataTypeSelect = document.getElementById('select-data');
 const searchInput = document.getElementById('search');
 const tooltip = document.querySelector('.tooltip');
 
-let dataType = dataTypeSelect.value;
-
 // set up empty maps for our two datasets
 const population = d3.map();
-const vote_count = d3.map();
 
 // geographic path generator
 const path = d3.geoPath();
@@ -28,9 +24,6 @@ const popFormat = d3.format(',');
 const popScale = d3.scaleLog()
     .domain([1,2000000])
     .range([0,1]);
-const turnoutScale = d3.scaleLinear()
-    .domain([0, 100])
-    .range([0, 1]);
 const color = d3.scaleQuantize()
     .domain([0,1])
     .range(scaleChromatic.schemeGnBu[9]);
@@ -45,34 +38,10 @@ d3.queue()
         population: parseInt(d.pop2016.replace(/,/g,''))
       });
 		})
-    .defer(d3.csv, '../data/votes.csv', (d) => {
-      vote_count.set(d.id, d.votes);
-    })
     .await(ready);
 
-// get either population or turnout data
-function getScaledValue(data) {
-  const county_population = population.get(data.id).population;
-  if (dataType === 'population') {
-    return popScale(county_population);
-  }
-  else {
-    var turnout = vote_count.get(data.id)/county_population * 100;
-    return turnoutScale(turnout);
-  }
-}
-
 function updateTooltip(county) {
-  let value;
-  if (dataType === 'population') {
-    value = popFormat(county.population);
-  }
-  else {
-    const county_votes = vote_count.get(county.id);
-    value = Math.round(parseInt(county_votes)/county.population * 100) + '%';
-  }
-
-  tooltip.innerHTML = `The ${dataType} of ${county.name}, ${county.state} is ${value}`;
+  tooltip.innerHTML = `The population of ${county.name}, ${county.state} is ${popFormat(county.population)}`;
 }
 
 function ready(error, counties) {
@@ -83,7 +52,7 @@ function ready(error, counties) {
     .selectAll('path')
       .data(topojson.feature(counties, counties.objects.counties).features)
       .enter().append('path')
-        .attr('fill', (d) => color(getScaledValue(d)))
+        .attr('fill', (d) => color(popScale(population.get(d.id).population)))
         .attr('d', path)
         .on('click', (e) => {
           updateTooltip(population.get(e.id))
@@ -115,10 +84,4 @@ function ready(error, counties) {
 
 		updateTooltip(county);
 	});
-
-  // add select change listener
-  dataTypeSelect.addEventListener('change', (e) => {
-    dataType = dataTypeSelect.value;
-    const paths = svg.selectAll('.counties path').attr('fill', (d) => color(getScaledValue(d)));
-  });
 }
